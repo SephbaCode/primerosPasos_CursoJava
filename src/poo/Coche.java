f26e521d608fb10e4e79471c38d8dab50b849f69
SephbaCode <mateobate152@gmail.com>
2023-01-09 14:32:58 -0500

    ejemplos de array bidimensionale sy uso del bucle for each

---
+++ src/poo/Coche.java
@@ -0,0 +1,22 @@
+package poo;
+
+//para una sintaxis mas comoda nombrar la clase con mayusculas la primera letra
+public class Coche {
+
+    int ruedas;
+    int largo,ancho;
+    int motor;
+    int peso;
+
+    //metodo constructor
+    public Coche(){
+        ruedas = 4;
+        largo = 300;
+        ancho = 150;
+        motor = 1600;
+        peso = 500;
+
+    }
+
+
+}
---
+++ src/poo/coche1.java
@@ -0,0 +1,16 @@
+package poo;
+
+public class coche1 {
+
+    public static void  main(String[] args){
+
+        //instanciar una clase.... un ejemplar de clase
+        Coche Renault = new Coche();
+
+        System.out.println("El coche tiene " + Renault.ruedas + " ruedas.");
+    }
+
+
+
+
+}
