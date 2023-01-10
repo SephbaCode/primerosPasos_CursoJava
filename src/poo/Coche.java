59373c96bed68347662280254af1130bb410739d
SephbaCode <mateobate152@gmail.com>
2023-01-10 10:52:00 -0500

    encapsulacion de datos y funciones setters y getters

--- src/poo/Coche.java
+++ src/poo/Coche.java
@@ -2,11 +2,17 @@ package poo;
 
 //para una sintaxis mas comoda nombrar la clase con mayusculas la primera letra
 public class Coche {
+//vamos a realizar la encapsulacion
+    final private int ruedas;
+    final private int largo,ancho;
+    final private int motor;
+    final private int peso_Plataforma;
 
-    int ruedas;
-    int largo,ancho;
-    int motor;
-    int peso;
+// estas pueden variar
+    private String color;
+    //private int peso_total;
+    private boolean asientosCuero;
+    //private boolean limatizador
 
     //metodo constructor
     public Coche(){
@@ -14,8 +20,55 @@ public class Coche {
         largo = 300;
         ancho = 150;
         motor = 1600;
-        peso = 500;
+        peso_Plataforma = 500;
+    }
+    // public para acceder a este metodo desde otras clases
+    // String por el tipo de dato que va a devolver, en este caso necesito una frase
+    //nombre del metodo getter
+    public String dime_largo(){
+        //return obligatorio para una funcion getter
+        //en esta clase, la variable largo se puede usar sin restricciones
+        return "El largo del coche es " + largo;
+    }
+//metodos getters
+    public String dime_ancho(){
+        return "El ancho del coche es " + ancho;
+    }
+    public String dime_ruedas(){
+        return "El coche tiene " + ruedas + " ruedas.";
+    }
+    public String dime_peso_Plataforma(){
+        return "El coche pesa " + peso_Plataforma + " kg.";
+    }
+    public String dime_motor(){
+        return "El motor del coche es de " + motor + " .";
+    }
+    public String dime_color(){
+        return "El color del coche es " + color + " .";
+    }
+    public String dimeDatosGenerales(){
+        return "El coche tiene un motor de capacidad " + motor + ", un largo de " +
+                largo + " y ancho de "+ancho+". Con "+ruedas+" ruedas y pesa "+peso_Plataforma+
+                ".";
+    }
+    public String dimeAsientos(){
+        if (asientosCuero){
+            return "Tiene asientos de cueros";
+        }else{
+            return "NO tiene asientos de cuero";
+        }
+    }
 
+    //metodos setters
+    //public para acceder desde otras clases, void por que no devuelve nada, no necesita return
+
+    public void establece_color(String colorEntrante){
+        color = colorEntrante;
+    }
+
+    public void configurAsientos(String asientosCuero){
+        //simplificacion del if else pues el metodo equals devuelve un boolean
+        this.asientosCuero = asientosCuero.equals("si");
     }
 
 
--- src/poo/coche1.java
+++ src/poo/coche1.java
@@ -1,13 +1,32 @@
 package poo;
 
+import javax.swing.*;
+
 public class coche1 {
 
     public static void  main(String[] args){
 
         //instanciar una clase.... un ejemplar de clase
-        Coche Renault = new Coche();
+        Coche miCoche = new Coche();
 
-        System.out.println("El coche tiene " + Renault.ruedas + " ruedas.");
+        //establecer color mediante paso de parametros
+        miCoche.establece_color(JOptionPane.showInputDialog("Introduzca color"));
+
+        //es importante encapsular esto para evitar modificar el dato desde donde no se deberia
+        //por lo que esta parte esta incorrecta, se debe usar el metodo correspondienta para presentar datos
+        //System.out.println("El coche tiene " + miCoche.ruedas + " ruedas.");
+        System.out.println(miCoche.dime_largo());
+        System.out.println(miCoche.dime_ancho());
+        System.out.println(miCoche.dime_ruedas());
+        System.out.println(miCoche.dime_peso_Plataforma());
+        System.out.println(miCoche.dime_motor());
+        System.out.println(miCoche.dime_color());
+        System.out.println(miCoche.dimeDatosGenerales());
+
+        miCoche.configurAsientos(JOptionPane.showInputDialog("Asientos de cuero?"));
+        System.out.println(miCoche.dimeAsientos());
+
+
     }
 
 
